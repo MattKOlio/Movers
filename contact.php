7e6bbee3268c1bafef5ff549fa40c40ee09bd640
Matthew Olio <mjosifovic227@gmail.com>
2021-09-13 07:21:53 +0200

    final update

--- contact.php
+++ contact.php
@@ -9,20 +9,20 @@ $phone = $_POST['phone'];
 $email = $_POST['email'];
 $size = $_POST['size'];
 
-$email_from = "mattew.lsf.801@gmail.com";
+$email_from = "matthew.lsf.801@gmail.com";
 $email_subject = "New Quote Request";
-$email_body = "Name: $first_name $last_name.\n".
+$email_body = "Name: $first_name $last_name\n".
                 "Moving from: $from\n".
                 "Moving To: $to\n".
                 "Moving Date: $date\n".
                 "Contact Number: $phone\n". 
                 "Contact Email: $email\n". 
                 "Move Size: $size\n";
-$email_to = "matthew.lsf.801@gmail.com";
+$email_to = "mjosifovic227@gmail.com";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $email\r\n";
 
-mail($email_to, $email_subject, $email_body, $headers);
+mail($email_to, $email_subject, $email_body);
 
 header("Location: contact.html");
 ?>
